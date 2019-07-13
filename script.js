c5cfaf7def7ff3fff197cc8c3e62ffcfa45b5228
zhs007 <sssxueren@gmail.com>
2019-07-13 13:34:34 +0800

    upd

--- script.js
+++ script.js
@@ -45,6 +45,10 @@ async function run() {
   // Train the model
   await trainModel(model, inputs, labels);
   console.log('Done Training');
+
+  // Make some predictions using the model and compare them to the
+  // original data
+  testModel(model, data, tensorData);
 }
 
 document.addEventListener('DOMContentLoaded', run);
@@ -129,4 +133,45 @@ async function trainModel(model, inputs, labels) {
       { height: 200, callbacks: ['onEpochEnd'] }
     )
   });
+}
+
+function testModel(model, inputData, normalizationData) {
+  const { inputMax, inputMin, labelMin, labelMax } = normalizationData;
+
+  // Generate predictions for a uniform range of numbers between 0 and 1;
+  // We un-normalize the data by doing the inverse of the min-max scaling
+  // that we did earlier.
+  const [xs, preds] = tf.tidy(() => {
+    const xs = tf.linspace(0, 1, 100);
+    const preds = model.predict(xs.reshape([100, 1]));
+
+    const unNormXs = xs.mul(inputMax.sub(inputMin)).add(inputMin);
+
+    const unNormPreds = preds.mul(labelMax.sub(labelMin)).add(labelMin);
+
+    // Un-normalize the data
+    return [unNormXs.dataSync(), unNormPreds.dataSync()];
+  });
+
+  const predictedPoints = Array.from(xs).map((val, i) => {
+    return { x: val, y: preds[i] };
+  });
+
+  const originalPoints = inputData.map(d => ({
+    x: d.horsepower,
+    y: d.mpg
+  }));
+
+  tfvis.render.scatterplot(
+    { name: 'Model Predictions vs Original Data' },
+    {
+      values: [originalPoints, predictedPoints],
+      series: ['original', 'predicted']
+    },
+    {
+      xLabel: 'Horsepower',
+      yLabel: 'MPG',
+      height: 300
+    }
+  );
 }
