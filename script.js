89433bd8704de0e028882fca7b2bae98243e3310
zhs007 <sssxueren@gmail.com>
2019-07-13 16:32:23 +0800

    upd

--- script.js
+++ script.js
@@ -60,23 +60,23 @@ function createModel() {
   const model = tf.sequential();
 
   // Add a single hidden layer
-  // model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true }));
-  model.add(
-    tf.layers.dense({
-      inputShape: [1],
-      units: 1,
-      useBias: true,
-      activation: 'sigmoid'
-    })
-  );
+  model.add(tf.layers.dense({ inputShape: [1], units: 50, useBias: true }));
+  // model.add(
+  //   tf.layers.dense({
+  //     inputShape: [1],
+  //     units: 1,
+  //     useBias: true,
+  //     activation: 'sigmoid'
+  //   })
+  // );
   // model.add(tf.layers.dense({ inputShape: [1], units: 50, activation: 'sigmoid' }));
   // model.add(tf.layers.dense({ units: 50, activation: 'sigmoid' }));
 
   // Add an output layer
-  // model.add(tf.layers.dense({ units: 1, useBias: true }));
-  model.add(
-    tf.layers.dense({ units: 1, useBias: true, activation: 'sigmoid' })
-  );
+  model.add(tf.layers.dense({ units: 1, useBias: true }));
+  // model.add(
+  //   tf.layers.dense({ units: 1, useBias: true, activation: 'sigmoid' })
+  // );
 
   return model;
 }
